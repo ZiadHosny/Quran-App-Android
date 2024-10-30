9af2c370496b41e96ae430607e745ec26313b33a
Ziad Hosny <ziadhosny007@gmail.com>
2024-10-30 09:51:31 +0300

    add all new code

---
+++ app/src/main/java/app/vercel/quran_app_z/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.quran_app_z;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
